4bbceb369f815729dfe4e51dc61263750a7f61d2
Julien Simonet <j.mons54@gmail.com>
2018-04-17 01:04:45 +0200

    fix

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,14 @@
 import Plugin from './Plugin.vue'
-import VueCodemirror from 'vue-codemirror'
-import 'codemirror/mode/vue/vue'
 
-module.exports = {
-  install: function (Vue, options = {
-    theme: 'material',
-    tabSize: 2,
-    lineNumbers: true,
-    mode: 'text/x-vue',
-  }) {
-    Vue.use(VueCodemirror, options)
-    Vue.component(options.name || 'live-preview', Plugin)
+function install(Vue, options) {
+  Vue.component('live-preview', Plugin)
+}
+
+export default install;
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(install);
+  if (install.installed) {
+    install.installed = false;
   }
 }
